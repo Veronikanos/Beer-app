3016bf4e747b28f77eadcf0412fa76abff0afc82
Veronica <vveronikanika@gmail.com>
2023-07-15 19:03:27 +0300

    feat: render only 15 recipes per page

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,24 +1,26 @@
-import {useEffect} from 'react';
+import {useEffect, useMemo} from 'react';
 import {useBeerStore} from '../store/store';
 import {RecipeItem} from './RecipeItem';
 
 export const RecipesList = () => {
   const beers = useBeerStore((state) => state.recipes);
   const fetchRecipes = useBeerStore((state) => state.fetchRecipes);
-  //  const increase = useBeerStore((state) => state.increase)
-  // return <><h1>{beers} around here ...</h1>
-  // <button onClick={increase}>one up</button></>
 
   useEffect(() => {
     fetchRecipes();
   }, [fetchRecipes]);
 
+//   const renderedItemsPerPage = beers.slice(0, 15);
+//   console.log(renderedItemsPerPage);
 
+  const visibleItems = useMemo(
+    () => beers.slice(0, 15),
+    [beers] );
 
   return (
     <ol>
       <p>Beer list is here:</p>
-      {beers.map((item) => (
+      {visibleItems.map((item) => (
         <RecipeItem id={item.id} name={item.name} description={item.description}
         />
       ))}
