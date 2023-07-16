30b596cd7e4816b8f6e3aa6176c02cbe9603a3be
Veronica <vveronikanika@gmail.com>
2023-07-16 19:02:57 +0300

    feat: add IntersectionObserver to modify render

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -1,15 +1,60 @@
-export const RecipeItem = ({recipe, handleRightMouseClick, isSelected, handleClick}) => {
+import { useEffect, useRef } from "react";
+
+export const RecipeItem = ({recipe, handleRightMouseClick, isSelected, handleClick, idx}) => {
 
     const itemStyle = {
         border: isSelected ? '1px solid blue' : '1px solid black',
-        backgroundColor: isSelected ? 'yellow' : '#ffffff'
+        backgroundColor: isSelected ? 'yellow' : '#ffffff',
+        height: "calc(100vh / 5)",
+        cursor: "pointer"
       };
 
-  return (
+      const descriptionStyle = {
+        width: "50%",
+        textOverflow: "ellipsis",
+        whiteSpace: "nowrap",
+        overflow: "hidden"
+      }
+
+    //   const lastElement = useRef(null);
+
+    //   useEffect(() => {
+    //     const observer = new IntersectionObserver((entries) => {
+    //       entries.forEach((entry) => {
+    //         if (entry.isIntersecting) {
+
+    //         console.log(  "test");
+    //         }
+    //       });
+    //     });
+      
+    //     if (lastElement.current) {
+    //       observer.observe(lastElement.current);
+    //     }
+      
+    //     // Зупинка спостереження при розмонтуванні компонента
+    //     return () => {
+    //       if (lastElement.current) {
+    //         observer.unobserve(lastElement.current);
+    //       }
+    //     };
+    //   }, []);
+
+    //   if (idx === 14){
+    //     return (
+    //     <li ref={lastElement} onContextMenu={(e) => handleRightMouseClick(e, recipe)} onClick={() => handleClick(recipe)}
+    //         style={itemStyle}>
+    //       <h3>{recipe.name}</h3>
+    //       <p style={descriptionStyle}>{recipe.description}</p>
+    //     </li>
+    //   )}else {
+          return (
     <li onContextMenu={(e) => handleRightMouseClick(e, recipe)} onClick={() => handleClick(recipe)}
         style={itemStyle}>
-      {recipe.name}
-      <p>{recipe.description}</p>
+      <h3>{recipe.name}</h3>
+      <p style={descriptionStyle}>{recipe.description}</p>
     </li>
   );
+    //   }
+
 };
--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -53,35 +53,99 @@ const RecipesList = () => {
   let navigate = useNavigate();
 
   const handleClick = (recipe) => {
-    // if (!selectedRecipes.includes(recipe)) {
-    //     setSelectedRecipes([...selectedRecipes, recipe]);
-    //   } else {
         setSelectedRecipes(
           selectedRecipes.filter((selectedRecipe) => selectedRecipe !== recipe)
         );
-    //   }
     setDetailedRecipe(recipe);
     navigate(`/recipes/${recipe.id}`);
   }
 
+// const options = {
+//     root: document.querySelector("ol").lastElementChild,
+//     rootMargin: '0px',
+//     threshold: 1.0
+//   }
+  
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (isInitialFetchDone) {
+        const observer = new IntersectionObserver(
+        ([entry]) => { // <- first intersection
+            setIsIntersecting(entry.isIntersecting);
+            console.log("isIntersecting:", entry.isIntersecting);
+
+        }, 
+        
+        // { rootMargin: "-300px" }
+      );
+    //   observer.observe(ref.current);
+  
+    
+      if (ref.current) {
+        console.log("ref.current:", ref.current);
+        observer.observe(ref.current);
+      }
+      
+      return () => {
+        if (ref.current) {
+            observer.unobserve(ref.current);
+          }
+      }  }
+  }, [isInitialFetchDone]);
+
+  useEffect(() => {
+    if (isIntersecting) {
+//       const el = ref.current.querySelector("ol").lastElementChild;
+      console.log(ref);
+    }
+  }, [isIntersecting]);
+
+   
+//   const callback = (entries, observer) => {
+//     entries.forEach(entry => {
+//       // do something
+//     })
+//   }
+   
+//   const observer = new IntersectionObserver(callback, options)
+
+// const ref = useRef(null);
+
+// useEffect(() => {
+//     const observer = new IntersectionObserver(entries => {
+//       if (entries[0].isIntersecting) {
+//        console.log("intersecting");
+//         }
+//     });
+//     observer.observe(ref.current);
+//   }, []);
+
   return (
     <div>
         {console.log(recipes)}
+
        {selectedRecipes.length > 0 && (
         <button onClick={handleDeleteButtonClick}>Delete</button>
       )}
+    <h2>Beer list is here:</h2>
+    <p>Click <i>left</i> mouse button to see recipe page</p>
+    <p>Click <i>right</i> mouse button to select/unselect recipe</p>
+
       <ol>
-        <p>Beer list is here:</p>
-        {visibleItems.map((item) => (
-          <RecipeItem
-            key={item.id}
+        {visibleItems.map((item, idx) => (
+          
+          <RecipeItem             key={item.id}
             recipe={item}
             handleRightMouseClick={handleRightMouseClick}
             handleClick={handleClick}
             isSelected={selectedRecipes.includes(item)}
+            idx={idx}
           />
         ))}
       </ol>
+      <div ref={ref} />
     </div>
   );
 };
